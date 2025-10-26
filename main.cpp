d52e00753754af32b0525b3acee7006fc361eeb0
Annie <annmora21@gmail.com>
2025-10-26 12:17:02 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
+// COMSC-210 | Lab 25 | Annie Morales
+// IDE used: Visual Studio Code
+
 #include <iostream>
 #include <chrono>
 using namespace std;
+using namespace std::chrono; // To use a clock.
 
 int main() {
-    
+    // To start timing
+    auto start = high_resolution_clock::now(); // Using clock of shortest tick period.
+
+
+
+
+
+
+
+    // To end timing
+    auto end = high_resolution_clock::now();
+
+    // To calculate duration 
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    // To output duration in milliseconds
+    cout << " " << duration.count() << "milliseconds\n";
 
     return 0;
 }
